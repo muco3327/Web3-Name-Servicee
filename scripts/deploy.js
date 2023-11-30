e8829d632319409ceeec054f2506435f8643b728
Mucahit <98721802+muco3327@users.noreply.github.com>
2023-11-30 22:58:51 +0300

    deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy("web3");
+    const domainContract = await domainContractFactory.deploy("dym");
     await domainContract.deployed();
   
     console.log("Contract deployed to:", domainContract.address);
